85f287b7c638062719c3d0d91e4971b3413ffb08
kuhnadri <adrian.kuhn@exxeta.com>
2020-08-28 12:28:54 +0200

    Initialise gh-pages branch

--- project/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Building jar..."
-mvn package
-
-echo "Dockerbuild..."
-docker build --build-arg JAR_FILE=target/*jar-with-dependencies.jar --tag java-k8s-operator:0.0.1-SNAPSHOT .
--- project/src/main/java/com/exxeta/java/k8s/operator/utility/SystemWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.exxeta.java.k8s.operator.utility;
-
-public class SystemWrapper {
-
-    public String getenv(String name){
-        return System.getenv(name);
-    }
-}
