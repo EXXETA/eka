f6e145cfcb721219fc7d8e9da73118c70c181dce
adrian1703 <65605180+adrian1703@users.noreply.github.com>
2020-09-14 12:17:54 +0200

    Update build.sh

--- project/build.sh
+++ project/build.sh
@@ -4,4 +4,4 @@ echo "Building jar..."
 mvn package
 
 echo "Dockerbuild..."
-docker build --build-arg JAR_FILE=target/*jar-with-dependencies.jar --tag java-k8s-operator:0.0.1-SNAPSHOT .
+docker build --build-arg JAR_FILE=target/*jar-with-dependencies.jar --tag eka:0.0.1 .
